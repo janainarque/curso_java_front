0daf1c708f61da3ee8b728e8a7245a0d1363a8c3
janainarque <janainarque@gmail.com>
2023-07-13 21:50:20 -0300

    aula de js - 13/07/2023

--- basico_JS/js/basico.js
+++ basico_JS/js/basico.js
@@ -1,6 +1,22 @@
 console.log("hello world, no arquivo js!");
 
-
 console.log(1 + "1")
-
 console.log(1 - "1")
+
+var x = 10
+let y = 11
+
+var x = 11
+
+
+console.log(typeof(x))
+
+console.log("aula" * 2)
+
+let numero = Number("50")
+let numeroString = String(50)
+let casasDecimais = 10.9988
+
+console.log(typeof(numero))
+console.log(typeof(numeroString))
+console.log(typeof(casasDecimais))
